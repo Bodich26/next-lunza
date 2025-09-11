35a9a260ec69ed0baff34a592c888d724d8f3be6
Bogdan <4634815@gmai.com>
2025-09-11 19:28:57 +0300

    Added custom colors and setting colors for dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <header>Header</header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Main
+        <div className="w-[200px] h-[200px] rounded-md bg-[var(--card-background)]">
+          main
+        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         Footer
